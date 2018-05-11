17b98d596b684606c6b0ad61ce29d2c4adb1c3e5
Novha <federicocabral10@hotmail.com>
2018-05-11 20:54:25 -0300

    Traducido el proyecto, Terminadas las historias 'Iniciar sesion' y 'Cerrar sesion'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-#  before_action :logged_in?, only: [:index, :edit, :update]
-#  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    def correct_user
+      redirect_to root_path unless params[:id] == current_user.id
+    end
 end
