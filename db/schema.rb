62d6253121a723be8cff407d28bffe6f000aa4f1
Novha <federicocabral10@hotmail.com>
2018-05-13 13:56:10 -0300

    Arreglar migracion telefono

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513012447) do
+ActiveRecord::Schema.define(version: 20180513165442) do
 
   create_table "trips", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180513012447) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "telefono"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
