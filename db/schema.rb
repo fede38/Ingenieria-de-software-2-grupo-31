d8d0623760ed22c89e272eaf9d002310c0f88c4d
Novha <federicocabral10@hotmail.com>
2018-05-12 22:30:16 -0300

    Migrar telefonos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513011449) do
+ActiveRecord::Schema.define(version: 20180513012447) do
 
   create_table "trips", force: :cascade do |t|
     t.datetime "created_at",  null: false
