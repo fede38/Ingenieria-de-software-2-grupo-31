9f728e01ee3a79a563b0f9accdde1b05bb69c241
Novha <federicocabral10@hotmail.com>
2018-05-12 21:45:45 -0300

    Enviado de mail de confirmacion

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  #Configuracion de enviador de mails
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "federicocabral101@gmail.com",
+    :password => "cumbianegra1",
+  }
 end
