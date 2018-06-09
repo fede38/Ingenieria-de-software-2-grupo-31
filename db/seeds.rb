ad8d583252a6ede03f68451c96dd6af985e01360
Novha <federicocabral10@hotmail.com>
2018-06-08 22:55:22 -0300

    Agregar carga de usuarios, vehiculos y asignar vehiculos a usuario y agregar atributo activo a tabla viajes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,91 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+nombres = [
+  'Isidro', 'Carmelo', 'Gerardo',
+  'Gustavo', 'Iker', 'Manuel',
+  'Antonio', 'Jose',' Maria', 'Juan'
+]
+apellidos = [
+  'Viva', 'Santana', 'Famadas', 'Garcia',
+  'Toledo', 'Malaguilla', 'Lopez',
+  'Fernandez', 'Rodriguez', 'Gonzalez'
+]
+marcas = [
+  'Mitsubishi', 'Fiat', 'Chevrolet',
+  'Citroen', 'Audi', 'Volkswagen',
+  'Ferrari', 'Lamborghini', 'Ford',
+  'Mercedes benz'
+]
+colores = [
+  'Azul', 'Rojo', 'Rosa', 'Amarillo',
+  'Verde', 'Naranja', 'Violeta',
+  'Blanco', 'Negro', 'Gris'
+]
+destinos = [
+  'La plata', 'Buenos aires', 'Olavarria',
+  'Azul', 'Pinamar', 'Rosario', 'Salta',
+  'Bariloche', 'Resistencia', 'Trelew',
+  'Posadas', 'Bahia blanca', 'Las flores',
+  'Rio cuarto', 'Puerto madryn'
+]
+
+10.times do |u|
+  user = User.new(
+    nombre: nombres[u],
+    apellido: apellidos[u],
+    email: apellidos[u].downcase+'@hotmail.com',
+    fecha_nacimiento: "199"+u.to_s+"-01-01",
+    password: '123456',
+    password_confirmation: '123456'
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+10.times do |a|
+  auto = Vehicle.new(
+    patente: [*('a'..'z'),*('0'..'9')].shuffle[0,6].join,
+    modelo: 1900+a*10,
+    marca: marcas[a],
+    sub_marca: rand(1..1000),
+    cantidad_asientos: rand(1..10),
+    color: colores[a],
+    tipo: 'Auto',
+  )
+  auto.save!
+end
+
+15.times do |v|
+  viaje = Trip.new(
+    descripcion: "Esta es la descripcion numero: "+ v.to_s + ".",
+    fecha_inicio: "2018-"+rand(7..12).to_s+"-"+rand(1..31).to_s,
+    hora_inicio: rand(00..23).to_s+":"+rand(00..59).to_s,
+    costo: rand(1000.0...5000.0),
+    destino: destinos[v],
+    activo: rand(0..1) == 1 ? true : false,
+  )
+  viaje.save!
+end
+
+User.all.each do |user|
+  10.times do |valor|
+    if rand(1..10) < 3
+      autoRandom = Vehicle.find(rand(1..10))
+      unless user.vehicles.exists?(:id => autoRandom.id)
+        user.vehicles << autoRandom
+      end
+    end
+  end
+end
+
+#15.times do |i|
+#  viaje = Trip.find(i+1)
+#  posicion = $usuariosConVehiculo.sample
+#  viaje.piloto = User.find(posicion)
+#  viaje.auto = User.find(posicion).vehicles.all.sample
+#  User.all.each do |usuario|
+#    if viaje.piloto != usuario
+#      if rand(1..10) == 1
+#        viaje.pasajeros << usuario
+#      end
+#    end
+#  end
+#end
