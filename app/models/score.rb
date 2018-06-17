60e276fd42cafdf6c78027f094c7f07bf02151d8
Pau Duran <37670227+paumduran@users.noreply.github.com>
2018-06-17 20:25:12 -0300

    controlador de Scores

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,4 @@
 class Score < ApplicationRecord
-	belongs_to :calificado, :class_name => 'User', foreign_key: 'user_id'
-	belongs_to :creador, :class_name => 'User', foreign_key: 'user_id'
+	belongs_to :calificado, :class_name => 'User', foreign_key: 'calificado_id'
+	belongs_to :creador, :class_name => 'User', foreign_key: 'creador_id'
 end
