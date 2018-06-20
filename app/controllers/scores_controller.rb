959144f72fd9deb7570534b5881b74f424efd18a
Novha <federicocabral10@hotmail.com>
2018-06-20 19:02:07 -0300

    Arreglar problema de ruteo

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,9 +1,10 @@
 class ScoresController < ApplicationController
 	def showCalifARealizar
+    @user = User.find(params[:id])
 	end
 
 	def showCalificaciones
-		@user= User.find(params[:user_id])
+		@user = User.find(params[:id])
 		@calification = Score.where(calificado: @user, realizada: true).order(fecha: :desc, hora: :desc)
 	end
 end
