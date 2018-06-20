2cf7308ff5a5e25f120b873aebbfb032b0c35c88
Pau Duran <37670227+paumduran@users.noreply.github.com>
2018-06-20 18:54:56 -0300

    espacio en mostrar viaje particular

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,9 +1,13 @@
 class ScoresController < ApplicationController
+	
 	def showCalifARealizar
+		@user = User.find(params[:user_id])
+		@calification = Score.where(creador: @user, realizada: false).order(fecha: :desc, hora: :desc)
 	end
 
 	def showCalificaciones
 		@user= User.find(params[:user_id])
-		@calification = Score.where(calificado: @user, realizada: true).order(fecha: :desc, hora: :desc)
+		@calif_piloto = Score.where(calificado: @user, realizada: true, tipo_calificacion: 'p').order(fecha: :desc, hora: :desc)
+		@calif_copiloto = Score.where(calificado: @user, realizada: true, tipo_calificacion: 'c').order(fecha: :desc, hora: :desc)
 	end
 end
