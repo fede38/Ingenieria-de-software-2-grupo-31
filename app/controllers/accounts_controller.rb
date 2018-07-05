faa8d80468a55341b4e6a041d2f9bf2b554d0384
Pau Duran <37670227+paumduran@users.noreply.github.com>
2018-07-05 19:32:49 -0300

    vista de cuenta

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,5 +2,7 @@ class AccountsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @cuenta = @user.account
+    r = Trip.where(activo: false).select(:trip_id)
+    @deudas = Embarkment.where(user_id: @user, estado: 'a', trip_id: r)
   end
 end
