0d4238c5a95fbe92cb9fac67ebd519c100761bf3
Pau Duran <37670227+paumduran@users.noreply.github.com>
2018-07-05 23:17:10 -0300

    sin false en seeds

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,7 +3,7 @@ class AccountsController < ApplicationController
     @user = User.find(params[:user_id])
     @cuenta = @user.account
     r = Trip.where(activo: false).select(:trip_id)
-    @deuda_piloto = Trip.where(piloto: @user, pagado: false, id: r)
+    @deuda_piloto = Trip.where(piloto: @user, pagado: false, activo: false)
     @deuda_copiloto = Embarkment.where(user_id: @user, estado: 'a', trip_id: r).where.not(deuda: 0)
   end
 end
