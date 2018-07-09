753bbe0cda1785098818b569b395009fa2df5e12
Novha <federicocabral10@hotmail.com>
2018-07-09 18:26:24 -0300

    Nose

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
     end
     resources :accounts, only: [:index]
     resources :trips do
+      resources :questions do
+        resources :answers
+      end
       member do
         put :aceptar
         put :rechazar
